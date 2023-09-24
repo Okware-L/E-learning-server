ca1cd8c6babe3325993fb67d7dc65f634ad146ff
Okware-L <lewisokware@gmail.com>
2023-09-25 01:01:50 +0300

    app

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: ["https://e-learning-client-nine.vercel.app"],
+    origin: ["https://localhost:3000"],
     credentials: true,
   })
 );
